966465298e1c76e3851596a8374ae10856da15cd
alperen <alpertign@gmail.com>
2021-08-05 02:42:51 +0300

    Design SearchFragment layout

--- app/src/main/java/com/alpertign/themoviemanager/SearchFragment.kt
+++ app/src/main/java/com/alpertign/themoviemanager/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 
 class SearchFragment : Fragment() {
@@ -15,7 +16,11 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view =  inflater.inflate(R.layout.fragment_search, container, false)
+
+        view.recycler_view.showShimmer()
+
+        return view
     }
 
 
